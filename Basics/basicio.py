1284f868a75b1cb6a22a45a3ab0bf49b71cffd6e
Aman Bhargava <amanb2000@gmail.com>
2019-06-17 15:48:16 -0400

    Add shapes.py

--- Basics/basicio.py
+++ Basics/basicio.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('m2.png', 0) # flag can be 0, 1, -1 == grayscale, color, original
+img = cv2.imread('assets/m2.png', 0) # flag can be 0, 1, -1 == grayscale, color, original
 print(img) # if the images DNE, img will be None
 
 cv2.imshow('image', img)
@@ -10,7 +10,7 @@ cv2.waitKey(0) # the argument is the number of milliseconds before auto-close. 0
 
 cv2.destroyAllWindows()
 
-cv2.imwrite('aman_copy.png', img)
+cv2.imwrite('assets/aman_copy.png', img)
 
 im2_list = []
 
@@ -19,11 +19,17 @@ for i in range(0, img.shape[0]):
 	tmp = []
 	for j in range(0, img.shape[1]):
 		num = img[i][j]
+
+		# if num > 200:
+		# 	num = 255
+
+		num = 255-((255-num)/2)
+		# print(type(num))
 		tmp += [num]
 
 	im2_list += [tmp]
 
-im2 = np.asarray(im2_list)
+im2 = np.asarray(im2_list, dtype=np.uint8)
 
 print(im2.shape)
 print(img.shape)
